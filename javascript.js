611238e931be4662063c402f9bf0b209e0ea319c
André <andreglock@gmail.com>
2021-03-29 13:41:17 +0200

    done

---
+++ javascript.js
@@ -0,0 +1,17 @@
+console.log("this is an exercise in console logging");
+
+let variable = "This is the value.";
+let firstName = "André";
+let lastName = "Lorenzoni";
+
+console.log(firstName, lastName);
+
+let name = "Linda";
+let city = "Leipzig";
+let job = "full time pet";
+let age = "3";
+let weight = "6,9";
+
+/* console.log(name, "is", age, "years old, and lives in", city, "she is a", job, "and whights", weight, "kg."); */
+
+console.log(name + " is " + age + " years old, and lives in " + city + " she is a " + job + " and weights " + weight + " kg.");
