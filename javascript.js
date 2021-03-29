99f5e23d6fefad58c1c90277108cdb98e85e9b2e
André <andreglock@gmail.com>
2021-03-29 15:03:24 +0200

    replaced let with const

--- javascript.js
+++ javascript.js
@@ -1,16 +1,16 @@
 console.log("this is an exercise in console logging");
 
-let variable = "This is the value.";
-let firstName = "André";
-let lastName = "Lorenzoni";
+const variable = "This is the value.";
+const firstName = "André";
+const lastName = "Lorenzoni";
 
 console.log(firstName, lastName);
 
-let name = "Linda";
-let city = "Leipzig";
-let job = "full time pet";
-let age = "3";
-let weight = "6,9";
+const name = "Linda";
+const city = "Leipzig";
+const job = "full time pet";
+const age = "3";
+const weight = "6,9";
 
 /* console.log(name, "is", age, "years old, and lives in", city, "she is a", job, "and whights", weight, "kg."); */
 
